a509aa3348353cc6e1cc66f9315f1813f711cedd
Aniket Mahajan <007aniketmahajan@gmail.com>
2022-02-19 16:54:31 +0530

    React Hooks

---
+++ Vanilla JS/debouncing.js
@@ -0,0 +1,18 @@
+function getValue(){
+    console.log('Printing Value');
+}
+
+function debounce(fn,delay){
+    let Timer;
+    return function(...args){
+        if(Timer) clearTimeout(Timer);
+        Timer = setTimeout(() => {
+            fn(...args)
+        },delay)
+    }
+}
+const betterFunction  = debounce(getValue,2000);
+//Calling a lot of times but executing once;
+for(let i=0;i<5;i++){
+    betterFunction();
+}
